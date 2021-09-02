fcc5be61a8c2404473d112e85d91e3ff27ca0627
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-09-02 19:52:15 +0530

    Home page Complete

--- src/Components/HomeInner/Content.jsx
+++ src/Components/HomeInner/Content.jsx
@@ -1,12 +1,16 @@
 import styles from "../../Styles/Home.module.css";
 import GetAppIcon from "@material-ui/icons/GetApp";
 import Button from "@material-ui/core/Button";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
 export const Content = () => {
   return (
     <div className={styles.ContentWrapper}>
       <div className={styles.jsDiv}>
         <p className={styles.Js}> JAVASCRIPT </p>
       </div>
+
       <div className={styles.imgWrapperDiv}>
         <div className={styles.imgDiv}>
           <img src={window.location.origin + "/profile.jpeg"} alt="" />
@@ -24,6 +28,10 @@ export const Content = () => {
             Resume
           </Button>
         </div>
+      </div>
+      <div className={styles.iconsDiv}>
+        <GitHubIcon fontSize="inherit" />
+        <LinkedInIcon style={{ fontSize: "2.2rem" }} />
       </div>
     </div>
   );
--- src/Components/HomeInner/Nav.jsx
+++ src/Components/HomeInner/Nav.jsx
@@ -1,10 +1,19 @@
 import styles from "../../Styles/Home.module.css";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 
 export const Nav = () => {
   const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const breakPoint = 900;
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
 
   return (
     <div className={styles.Home}>
@@ -15,23 +24,35 @@ export const Nav = () => {
           <p> WEB DEVELOPER</p>
         </div>
       </div>
-      <div onClick={() => setOpen(true)}>
-        {" "}
-        <MenuIcon />{" "}
-      </div>
-      <SwipeableDrawer
-        anchor="right"
-        open={open}
-        onClose={() => {
-          setOpen(false);
-        }}
-        onOpen={() => {
-          setOpen(true);
-        }}
-      >
-        <h1> one </h1>
-        <h2> Two </h2>
-      </SwipeableDrawer>
+      {width < breakPoint ? (
+        <>
+          <div onClick={() => setOpen(true)}>
+            {" "}
+            <MenuIcon />{" "}
+          </div>
+          <SwipeableDrawer
+            anchor="right"
+            open={open}
+            onClose={() => {
+              setOpen(false);
+            }}
+            onOpen={() => {
+              setOpen(true);
+            }}
+          >
+            <h1> one </h1>
+            <h2> Two </h2>
+          </SwipeableDrawer>
+        </>
+      ) : (
+        <div className={styles.NavigationLinks}>
+          <p> Home </p>
+          <p> About </p>
+          <p> Proficiencies </p>
+          <p> Projects </p>
+          <p> Contacts </p>
+        </div>
+      )}
     </div>
   );
 };
