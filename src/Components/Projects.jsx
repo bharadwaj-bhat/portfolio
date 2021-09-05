0f5edeb2dd20507c0cf5d24e2f4dd4cbfdd4ddb2
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-09-05 23:07:54 +0530

    project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,7 @@ import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
 import AwsSliderStyles from "react-awesome-slider/dist/styles.css";
 import "../Styles/awesome-slider-override.css";
+import Divider from "@material-ui/core/Divider";
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 export const Projects = () => {
@@ -12,20 +13,26 @@ export const Projects = () => {
       <h1> Projects </h1>
       <div className={styles.sliderWrapper}>
         <div>
-          <h4
+          <div
             style={{
-              padding: "0px 0px",
-              marginBottom: "-15px",
+              height: "120px",
               marginTop: "20px",
             }}
           >
-            {" "}
-            <span style={{ fontWeight: "600" }}>Groww.in</span>
-          </h4>
-          <h4>
-            A financial website that allows users to buy and sell stocks with
-            just a click of a button.{" "}
-          </h4>
+            <h4
+              style={{
+                padding: "0px 0px",
+                marginBottom: "-15px",
+              }}
+            >
+              {" "}
+              <span>Groww.in</span>
+            </h4>
+            <h4>
+              A financial website that allows users to buy and sell stocks with
+              just a click of a button.{" "}
+            </h4>
+          </div>
           <div
             style={{
               display: "flex",
@@ -38,20 +45,164 @@ export const Projects = () => {
           >
             <p> Tech Stacks - </p>
             <img
-              style={{ width: "20px" }}
               src={window.location.origin + "/iconImages/htmlBlackIcon.png"}
               alt=""
             />{" "}
+            <img src={window.location.origin + "/iconImages/css.png"} alt="" />
             <img
-              style={{ width: "20px" }}
-              src={window.location.origin + "/iconImages/css.png"}
-              alt=""
-            />
-            <img
-              style={{ width: "20px", border: "1px solid black" }}
+              style={{ border: "1px solid black" }}
               src={window.location.origin + "/iconImages/javascript.png"}
               alt=""
             />
+          </div>
+          <div className={styles.buttonDiv}>
+            <button> Repo </button>
+            <button> Demo </button>
+            <button> Watch Demo </button>
+          </div>
+          <AutoplaySlider
+            cssModule={AwsSliderStyles}
+            infinite={true}
+            play={true}
+            cancelOnInteraction={true}
+            interval={2000}
+          >
+            <div
+              data-src={window.location.origin + "//growwImages/groww1.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww2.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww3.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww4.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww5.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww6.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww7.png"}
+            />
+          </AutoplaySlider>
+          <br />
+        </div>
+        <div>
+          <div
+            style={{
+              height: "120px",
+              marginTop: "20px",
+            }}
+          >
+            <h4
+              style={{
+                padding: "0px 0px",
+                marginBottom: "-15px",
+              }}
+            >
+              {" "}
+              <span>Milaap.org</span>
+            </h4>
+            <h4>
+              A humanitarian website that allows users to start a fundraiser and
+              as well as donate for other causes.{" "}
+            </h4>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              marginBottom: "20px",
+              marginTop: "-10px",
+              color: "#878a8f",
+            }}
+          >
+            <p> Tech Stacks - </p>
+            <img
+              src={window.location.origin + "/iconImages/reactBlueIcon.png"}
+              alt=""
+            />{" "}
+          </div>
+          <div className={styles.buttonDiv}>
+            <button> Repo </button>
+            <button> Demo </button>
+            <button> Watch Demo </button>
+          </div>
+          <AutoplaySlider
+            cssModule={AwsSliderStyles}
+            infinite={true}
+            play={true}
+            cancelOnInteraction={true}
+            interval={2000}
+          >
+            <div
+              data-src={window.location.origin + "//growwImages/groww1.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww2.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww3.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww4.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww5.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww6.png"}
+            />
+            <div
+              data-src={window.location.origin + "/growwImages/groww7.png"}
+            />
+          </AutoplaySlider>
+        </div>
+        <div>
+          <div style={{ height: "120px", marginTop: "20px" }}>
+            <h4
+              style={{
+                padding: "0px 0px",
+                marginBottom: "-15px",
+              }}
+            >
+              {" "}
+              <span>Travel Guru</span>
+            </h4>
+            <h4>
+              A Travel website that allows users to book homestays and hotels at
+              your desired destination.{" "}
+            </h4>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              marginBottom: "20px",
+              marginTop: "-10px",
+              color: "#878a8f",
+            }}
+          >
+            <p> Tech Stacks - </p>
+            <img
+              src={window.location.origin + "/iconImages/reactBlueIcon.png"}
+              alt=""
+            />{" "}
+            <img
+              src={window.location.origin + "/iconImages/reduxIcon.png"}
+              alt=""
+            />
+          </div>
+          <div className={styles.buttonDiv}>
+            <button> Repo </button>
+            <button> Demo </button>
+            <button> Watch Demo </button>
           </div>
           <AutoplaySlider
             cssModule={AwsSliderStyles}
