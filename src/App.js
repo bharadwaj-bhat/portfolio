167f0d4febfb456a26b520a02fbfc612e9166c41
Bharadwaj <sbhatbharadwaj24@gmail.com>
2021-09-06 23:54:23 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { About } from "./Components/About";
 import { Proficiencies } from "./Components/Proficiencies";
 import { Contact } from "./Components/Contact";
 import { Projects } from "./Components/Projects";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    document.title = "Bharadwaj Bhat";
+  }, []);
   return (
     <div className="App">
       <Home />
